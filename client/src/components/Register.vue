20df775baf1cc2d4bffb577648c0ac326d935789
Pedro F Thomaz <pfthomaz@gmail.com>
2018-04-23 14:25:53 +0100

    Setup of Sequelize and Vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,48 +1,69 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-app>
+    <v-layout column>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>Register</v-toolbar-title>
 
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-      <br>
-      <button
-        @click="register">
-        Register
-      </button>
-  </div>
+          </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <h1>Register</h1>
+
+            <input
+              type="email"
+              name="email"
+              v-model="email"
+              placeholder="email" />
+            <br>
+            <input
+              type="password"
+              name="password"
+              v-model="password"
+              placeholder="password" />
+              <br>
+              <div class="error" v-html="error" />
+              <v-btn
+                class="cyan"
+                dark
+                @click="register">
+                Register
+              </v-btn>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-app>
 </template>
 
 <script>
-import AuthenticationService from '@/services/AuthenticationService'
+import AuthenticationService from '@/services/AuthenticationService';
 export default {
   data () {
     return {
       email: '',
-      password: ''
-    }
+      password: '',
+      error: null
+    };
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color: red;
+}
 </style>
